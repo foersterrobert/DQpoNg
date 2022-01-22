917d132625d6bccc66b191405b6cb35cab575e44
robertfoerster <foersteroberthh@gmail.com>
2022-01-22 12:25:58 +0100

    standart ddqn

--- model.py
+++ model.py
@@ -7,23 +7,15 @@ class Linear_QNet(nn.Module):
         self.linear1 = nn.Linear(input_size, 128)
         self.linear2 = nn.Linear(128, 256)
         self.linear3 = nn.Linear(256, 512)
-        self.linear4 = nn.Linear(512, 2048)
-
-        self.v_stream = nn.Linear(2048, 512)
-        self.v = nn.Linear(512, 1)
-        self.a_stream = nn.Linear(2048, 512)
-        self.a = nn.Linear(512, output_size)
+        self.linear4 = nn.Linear(512, 1024)
+        self.linear5 = nn.Linear(1024, 512)
+        self.linear6 = nn.Linear(512, output_size)
 
     def forward(self, x):
         x = F.relu(self.linear1(x))
         x = F.relu(self.linear2(x))
         x = F.relu(self.linear3(x))
         x = F.relu(self.linear4(x))
-        
-        v = F.relu(self.v_stream(x))
-        v = self.v(v)
-        a = F.relu(self.a_stream(x))
-        a = self.a(a)
-
-        q = v + (a - a.mean(dim=1, keepdim=True))
-        return q
+        x = F.relu(self.linear5(x))
+        x = self.linear6(x)
+        return x
